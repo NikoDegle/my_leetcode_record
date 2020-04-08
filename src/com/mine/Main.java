4ea7362dafdb6696cbffa2155afefb587048a64f
wanghancong <cong1192047072@163.com>
2020-04-08 20:48:58 +0800

    第8题

--- src/com/mine/Main.java
+++ src/com/mine/Main.java
@@ -1,12 +1,12 @@
 package com.mine;
 
-import com.mine.questions.No6;
+import com.mine.questions.No8;
 import com.mine.questions.Question;
 
 public class Main {
 
     public static void main(String[] args) {
-        Question question = new No6();
+        Question question = new No8();
         question.run();
     }
 }
